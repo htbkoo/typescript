f27fbd1ea18609babc12b99a81dc44e9ad30d4ab
H.T. Koo <h1017760@connect.hku.hk>
2016-09-08 19:10:37 +0800

    Codewars - TypeScript - updated 1 (6Kyu - DiseaseSpread)
    --HG--
    branch : typescript

--- src/main/typescript/online/codewars/_6kyu/test/DiseaseSpread.Test.ts
+++ src/main/typescript/online/codewars/_6kyu/test/DiseaseSpread.Test.ts
@@ -12,23 +12,25 @@ function assertFuzzyEquals(actual, expected) {
 function testing(tm, n, s0, i0, b, a, expected) {
     assertFuzzyEquals(solution.G964.epidemic(tm, n, s0, i0, b, a), expected);
 }
-describe("Fixed Tests epidemic", function () {
-    it("Basic tests", function () {
-        var tm = 18, n = 432, s0 = 1004, i0 = 1, b = 0.00209, a = 0.51;
-        testing(tm, n, s0, i0, b, a, 420);
-        tm = 12;
-        n = 288;
-        s0 = 1007;
-        i0 = 2;
-        b = 0.00206;
-        a = 0.45;
-        testing(tm, n, s0, i0, b, a, 461);
-        tm = 13;
-        n = 312;
-        s0 = 999;
-        i0 = 1;
-        b = 0.00221;
-        a = 0.55;
-        testing(tm, n, s0, i0, b, a, 409);
-    });
+describe("DiseaseSpread", function () {
+    describe("Fixed Tests epidemic", function () {
+        it("Basic tests", function () {
+            var tm = 18, n = 432, s0 = 1004, i0 = 1, b = 0.00209, a = 0.51;
+            testing(tm, n, s0, i0, b, a, 420);
+            tm = 12;
+            n = 288;
+            s0 = 1007;
+            i0 = 2;
+            b = 0.00206;
+            a = 0.45;
+            testing(tm, n, s0, i0, b, a, 461);
+            tm = 13;
+            n = 312;
+            s0 = 999;
+            i0 = 1;
+            b = 0.00221;
+            a = 0.55;
+            testing(tm, n, s0, i0, b, a, 409);
+        });
+    })
 });
