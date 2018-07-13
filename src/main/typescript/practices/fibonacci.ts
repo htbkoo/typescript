679a547c7b8dfaf59d329033d48607454deebd78
htbkoo <h1017760@connect.hku.hk>
2018-07-13 20:14:50 +0800

    implemented simple fibonacci.ts typescript

--- src/main/typescript/practices/fibonacci.ts
+++ src/main/typescript/practices/fibonacci.ts
@@ -1,5 +1,10 @@
 function fibonacci(n: number): number {
-    return 0;
+    if (n <= 0) {
+        return 0;
+    } else if (n === 1) {
+        return 1
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
 export default fibonacci;
