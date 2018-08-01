bcc17226171f5ec0751b1a45672cd6c8258b1839
htbkoo <h1017760@connect.hku.hk>
2018-08-01 19:02:34 +0800

    refactored typescript

--- src/main/typescript/online/freeCodeCamp/javascriptAlgorithmsAndDataStructures/cashRegister.ts
+++ src/main/typescript/online/freeCodeCamp/javascriptAlgorithmsAndDataStructures/cashRegister.ts
@@ -1,9 +1,9 @@
 export type cidType = ReadonlyArray<[CoinNames, number]>;
-type CoinNames = keyof typeof AMOUNTS;
+type CoinNames = keyof typeof NORMALIZED_AMOUNTS;
 
 // Less generic and requires code changes
 // but make our lives much easier by sticking to int
-const AMOUNTS = {
+const NORMALIZED_AMOUNTS = {
     PENNY: 1,
     NICKEL: 5,
     DIME: 10,
@@ -16,7 +16,7 @@ const AMOUNTS = {
 };
 const MULTIPLIER = 100;
 
-const SORT_AMOUNT_KEYS = Object.keys(AMOUNTS).sort((a, b) => AMOUNTS[a] - AMOUNTS[b]);
+const SORT_AMOUNT_KEYS = Object.keys(NORMALIZED_AMOUNTS).sort((a, b) => NORMALIZED_AMOUNTS[a] - NORMALIZED_AMOUNTS[b]);
 
 const responseFactories = {
     INSUFFICIENT_FUNDS() {
@@ -50,18 +50,18 @@ function normalize(price) {
 
 function getCoinCounts(cid: cidType): Readonly<{ [k in CoinNames]?: number }> {
     return cid.reduce((prev, [coinName, totalAmount]) => {
-        prev[coinName] = Math.round(normalize(totalAmount) / AMOUNTS[coinName]);
+        prev[coinName] = Math.round(normalize(totalAmount) / NORMALIZED_AMOUNTS[coinName]);
         return prev;
     }, {});
 }
 
 function computeChangesTable(changeAvailable, requiredChange, coinCounts: Readonly<{ [k in CoinNames]?: number }>) {
-    let step = AMOUNTS[SORT_AMOUNT_KEYS[0]];
+    let step = NORMALIZED_AMOUNTS[SORT_AMOUNT_KEYS[0]];
     let changes = {0: {}};
     for (let i = step; (i <= (changeAvailable + step)) && (i <= (requiredChange + step)); i += step) {
         let possibleConfig = SORT_AMOUNT_KEYS.reduce((prev: any, key) => {
             // TODO: may optimize by terminating the loop earlier
-            let beforeCoinAmount = (i - AMOUNTS[key]);
+            let beforeCoinAmount = (i - NORMALIZED_AMOUNTS[key]);
             if (beforeCoinAmount in changes) {
                 let coinCount = 1 + ((key in changes[beforeCoinAmount]) ? changes[beforeCoinAmount][key] : 0);
 
@@ -97,7 +97,7 @@ function computeChangesTable(changeAvailable, requiredChange, coinCounts: Readon
     return changes;
 }
 
-const ORIGINAL_AMOUNTS = {
+const AMOUNTS = {
     PENNY: 0.01,
     NICKEL: 0.05,
     DIME: 0.1,
@@ -112,7 +112,7 @@ const ORIGINAL_AMOUNTS = {
 function createResponse(changes, requiredChange) {
     if (requiredChange in changes) {
         let list = changes[requiredChange];
-        let openChange = SORT_AMOUNT_KEYS.slice().reverse().filter(key => key in list).map(key => [key, list[key] * ORIGINAL_AMOUNTS[key]]);
+        let openChange = SORT_AMOUNT_KEYS.slice().reverse().filter(key => key in list).map(key => [key, list[key] * AMOUNTS[key]]);
         return responseFactories.OPEN(openChange);
     } else {
         return responseFactories.INSUFFICIENT_FUNDS();
