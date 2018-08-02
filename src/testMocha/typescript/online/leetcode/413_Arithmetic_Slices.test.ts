92059d0f3759ce94bd79ed3aa35527f5bf97732e
htbkoo <h1017760@connect.hku.hk>
2018-08-02 19:19:26 +0800

    added more test cases typescript

--- src/testMocha/typescript/online/leetcode/413_Arithmetic_Slices.test.ts
+++ src/testMocha/typescript/online/leetcode/413_Arithmetic_Slices.test.ts
@@ -5,7 +5,12 @@ import numberOfArithmeticSlices from "../../../../main/typescript/online/leetcod
 describe("413. Arithmetic Slices", function () {
     describe('should calculate number of arithmetic slices', function () {
         [
+            {A: [], expected: 0},
+            {A: [1], expected: 0},
             {A: [1, 2, 3, 4], expected: 3},
+            {A: [1, 1, 2, 5, 7], expected: 0},
+            {A: [1, 1, 2, 5, 7, 1, 3, 5, 7, 9, 7, 7, 7, 7, 3, -1, -5, -9], expected: 15},
+            {A: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], expected: 561},
         ].forEach(({A, expected}) =>
             it(`numberOfArithmeticSlices(${JSON.stringify(A)})=${expected}`, function () {
                 // given
