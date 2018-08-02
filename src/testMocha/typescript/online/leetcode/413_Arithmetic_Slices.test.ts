2e5e5301332ac0a7e96d59bf2b6fcc90ec0eb1ba
htbkoo <h1017760@connect.hku.hk>
2018-08-02 19:12:18 +0800

    created 413_Arithmetic_Slices.test.ts typescript

---
+++ src/testMocha/typescript/online/leetcode/413_Arithmetic_Slices.test.ts
@@ -0,0 +1,20 @@
+import * as chai from "chai";
+
+import numberOfArithmeticSlices from "../../../../main/typescript/online/leetcode/413_Arithmetic_Slices";
+
+describe("413. Arithmetic Slices", function () {
+    describe('should calculate number of arithmetic slices', function () {
+        [
+            {A: [1, 2, 3, 4], expected: 3},
+        ].forEach(({A, expected}) =>
+            it(`numberOfArithmeticSlices(${JSON.stringify(A)})=${expected}`, function () {
+                // given
+                // when
+                let actual = numberOfArithmeticSlices(A);
+
+                // then
+                chai.expect(actual).to.equal(expected);
+            })
+        );
+    });
+});
