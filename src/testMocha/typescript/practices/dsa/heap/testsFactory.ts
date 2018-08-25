d082418c0fba74124d5b51d75e58b7b7a1104df3
htbkoo <h1017760@connect.hku.hk>
2018-08-25 22:05:06 +0800

    made practices\dsa\heap\testsFactory.ts more flexible typescript

--- src/testMocha/typescript/practices/dsa/heap/testsFactory.ts
+++ src/testMocha/typescript/practices/dsa/heap/testsFactory.ts
@@ -1,17 +1,23 @@
 import * as chai from "chai";
 
 const heapTestCases = require("./resources/heapTestCases.json");
+const defaultOutputAssertion = (actual) => ({
+    matches: expected => expected.forEach((num, i) => {
+        let v = actual.pop();
+        chai.expect(v).to.equal(num, `wrong at index: ${i}, expected: <${num}> but was <${v}>`);
+    })
+});
 
-export default (heapClassName, newHeapOf) => heapTestCases.forEach(({input, expected}, index) =>
+export default (heapClassName, newHeapOf, transformInput?, assertOutput = defaultOutputAssertion) => heapTestCases.forEach(({input, expected}, index) =>
     it(`${heapClassName} - case ${index}: should sort by Heap Sort(${input})=${expected}`, function () {
         // given
         // when
+        if (transformInput) {
+            input = transformInput(input);
+        }
         let actual = newHeapOf(input);
 
         // then
-        expected.forEach((num, i) => {
-            let v = actual.pop();
-            chai.expect(v).to.equal(num, `wrong at index: ${i}, expected: <${num}> but was <${v}>`);
-        });
+        assertOutput(actual).matches(expected);
     })
 );
