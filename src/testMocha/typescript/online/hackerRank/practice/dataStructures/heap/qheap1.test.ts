dd2494f77ea9880ae3192d766b381c6d33453fc8
htbkoo <h1017760@connect.hku.hk>
2018-08-28 19:44:18 +0800

    added test for qheap1.test.ts typescript

---
+++ src/testMocha/typescript/online/hackerRank/practice/dataStructures/heap/qheap1.test.ts
@@ -0,0 +1,22 @@
+import * as chai from "chai";
+import qheap1 from "../../../../../../../main/typescript/online/hackerRank/practice/dataStructures/heap/qheap1";
+
+
+describe("QHEAP1", function () {
+    describe('should implement qheap1', function () {
+        [
+            {
+                input: `5\n1 4\n1 9\n3\n2 4\n3`, output: "4\n9"
+            },
+        ].forEach(({input, output}) =>
+            it(`qheap1(${JSON.stringify(input)})=${JSON.stringify(output)}`, function () {
+                // given
+                // when
+                let actual = qheap1(input);
+
+                // then
+                chai.expect(actual).to.equal(output);
+            })
+        );
+    });
+});
