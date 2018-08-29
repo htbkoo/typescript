b0b535b8eebd2fca1553d04b34d94a3e7f746ade
htbkoo <h1017760@connect.hku.hk>
2018-08-29 09:04:16 +0800

    created 208_Implement_Trie_Prefix_Tree.test.ts typescript

---
+++ src/testMocha/typescript/online/leetcode/208_Implement_Trie_Prefix_Tree.test.ts
@@ -0,0 +1,33 @@
+import * as chai from "chai";
+
+import Trie from "../../../../main/typescript/online/leetcode/208_Implement_Trie_Prefix_Tree";
+
+describe("208. Implement Trie (Prefix Tree)", function () {
+    describe('should implement a trie with insert, search, and startsWith methods', function () {
+        [
+
+            {
+                command: ["insert", "search", "search", "startsWith", "insert", "search"],
+                args: [["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]],
+                output: [undefined, true, false, true, undefined, true]
+            },
+        ].forEach((testCase) =>
+            it(`KthLargest(${JSON.stringify(testCase)}`, function () {
+                // given
+                const {command, args, output} = testCase;
+
+                // when
+                let trie = new Trie();
+
+                // then
+                output.forEach((expected, i) => {
+                        let actual = trie[command[i]].apply(trie, args[i]);
+                        if (typeof expected !== "undefined") {
+                            chai.expect(actual).to.equal(expected, `wrong at position: ${i}`)
+                        }
+                    }
+                );
+            })
+        );
+    });
+});
