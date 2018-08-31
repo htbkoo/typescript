98aa06bba6ea8655664cf1e8e70aa2b94c49f7d0
htbkoo <h1017760@connect.hku.hk>
2018-08-31 19:28:34 +0800

    created test 745_Prefix_and_Suffix_Search.test.ts typescript

---
+++ src/testMocha/typescript/online/leetcode/745_Prefix_and_Suffix_Search.test.ts
@@ -0,0 +1,26 @@
+import * as chai from "chai";
+
+import WordFilter from "../../../../main/typescript/online/leetcode/745_Prefix_and_Suffix_Search";
+
+describe("745. Prefix and Suffix Search", function () {
+    describe('should return the word with given prefix and suffix with maximum weight', function () {
+        [
+            {
+                words: ["apple"],
+                prefixAndSuffix: [["a", "e"], ["b", ""]],
+                output: [0, -1]
+            },
+        ].forEach(({words, prefixAndSuffix, output}) =>
+            it(`replaceWords(${JSON.stringify(words)}, ${JSON.stringify(prefixAndSuffix)})=${output}`, function () {
+                // given
+                let wordFilter = new WordFilter(words);
+
+                // when
+                // then
+                output.forEach(expectedWeight =>
+                    chai.expect(wordFilter.f.apply(wordFilter, prefixAndSuffix)).to.equal(expectedWeight)
+                )
+            })
+        );
+    });
+});
