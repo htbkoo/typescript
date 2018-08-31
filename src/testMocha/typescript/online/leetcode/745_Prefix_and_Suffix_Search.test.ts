24dfb024b65cbcc832c49da1340e0ab83361763e
htbkoo <h1017760@connect.hku.hk>
2018-08-31 21:36:42 +0800

    added more test cases typescript

--- src/testMocha/typescript/online/leetcode/745_Prefix_and_Suffix_Search.test.ts
+++ src/testMocha/typescript/online/leetcode/745_Prefix_and_Suffix_Search.test.ts
@@ -10,6 +10,26 @@ describe("745. Prefix and Suffix Search", function () {
                 prefixAndSuffix: [["a", "e"], ["b", ""]],
                 output: [0, -1]
             },
+            {
+                words: ["apple", "alice"],
+                prefixAndSuffix: [["a", "e"], ["b", ""]],
+                output: [1, -1]
+            },
+            {
+                words: ["apple", "alice", "apple"],
+                prefixAndSuffix: [["a", "e"], ["b", ""]],
+                output: [2, -1]
+            },
+            {
+                words: ["apple", "alice", "apple", "bad", "alice"],
+                prefixAndSuffix: [["a", "e"], ["b", ""]],
+                output: [4, 3]
+            },
+            {
+                words: ["apple", "alice", "apple", "bad", "alice", "bed", "dog"],
+                prefixAndSuffix: [["a", "e"], ["b", ""]],
+                output: [4, 5]
+            },
         ].forEach(({words, prefixAndSuffix, output}) =>
             it(`replaceWords(${JSON.stringify(words)}, ${JSON.stringify(prefixAndSuffix)})=${output}`, function () {
                 // given
