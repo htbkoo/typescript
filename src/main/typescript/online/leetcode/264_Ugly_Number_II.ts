b3c52de5179e70a57eaaa65fb37196ec3ece60fa
htbkoo <h1017760@connect.hku.hk>
2018-09-08 10:40:35 +0800

    264_Ugly_Number_II.ts - actually second submission is not much quicker typescript

--- src/main/typescript/online/leetcode/264_Ugly_Number_II.ts
+++ src/main/typescript/online/leetcode/264_Ugly_Number_II.ts
@@ -23,113 +23,28 @@ Note:
  * @return {number}
  */
 var nthUglyNumber = function (n: number): number {
-    let heap = MinHeap.newHeap();
-    heap.push(1);
-    let count = 0, value, added = {1: true};
-    while (count < n) {
-        value = heap.pop();
-        count++;
-        [2, 3, 5].forEach(factor => {
-            let newValue = value * factor;
-            if (!(newValue in added)) {
-                heap.push(newValue);
-                added[newValue] = true;
+    // reference: http://fisherlei.blogspot.com/2015/10/leetcode-ugly-number-ii-solution.html
+    const arr = [1];
+    const pt = {2: 0, 3: 0, 5: 0};
+    while (arr.length < n) {
+        let keys = Object.keys(pt);
+        const values = keys.map(key => parseInt(key))
+            .reduce((obj, key: number) => {
+                obj[key] = arr[pt[key]] * key;
+                return obj;
+            }, {});
+
+        const min = keys.reduce((min, key) => Math.min(values[key], min), values[keys[0]]);
+        arr.push(min);
+
+        keys.forEach(key => {
+            if (min === values[key]) {
+                ++pt[key];
             }
         });
     }
 
-    return value;
+    return arr[n - 1];
 };
-
-export class MinHeap {
-    private readonly _values: number[] = [];
-
-    public static newHeap() {
-        return new MinHeap();
-    }
-
-    public static heapFrom(arr: number[]) {
-        return arr.reduce((heap, v) => {
-            heap.push(v);
-            return heap;
-        }, new MinHeap());
-    }
-
-    public push(value: number) {
-        this._values.push(value);
-        this._pullUp(this._size() - 1);
-    }
-
-    public pop(): number {
-        const size = this._size();
-        if (size === 0) {
-            throw new Error("heap is empty");
-        } else if (size === 1) {
-            return this._values.pop();
-        } else {
-            const value = this._values[0];
-            this._values[0] = this._values.pop();
-            this._pushDown(0);
-            return value;
-        }
-    }
-
-    private _size(): number {
-        return this._values.length;
-    }
-
-    private _pullUp(pos: number) {
-        const isRoot = pos === 0;
-        if (!isRoot) {
-            const parentPos = Math.floor((pos - 1) / 2);
-            if (this._isFirstBetter(pos, parentPos)) {
-                this._swap(parentPos, pos);
-                this._pullUp(parentPos);
-            }
-        }
-    }
-
-    private _isFirstBetter(first: number, second: number) {
-        return this._values[first] < this._values[second];
-    }
-
-    private _swap(pos: number, other: number) {
-        const temp = this._values[pos];
-        this._values[pos] = this._values[other];
-        this._values[other] = temp;
-    }
-
-    private _pushDown(pos: number) {
-        const numChild = this._numberOfChildren(pos);
-        if (numChild > 0) {
-            const betterChild = this._betterChild(pos, numChild);
-            if (this._isFirstBetter(betterChild, pos)) {
-                this._swap(betterChild, pos);
-                this._pushDown(betterChild);
-            }
-        }
-    }
-
-    private _numberOfChildren(pos: number): number {
-        return Math.min(2, Math.max(0, this._size() - (2 * pos - 1)))
-    }
-
-    private _betterChild(pos: number, numChild: number) {
-        const left = 2 * pos + 1;
-        switch (numChild) {
-            case 1:
-                return left;
-            case 2:
-                const right = 2 * pos + 2;
-                if (this._isFirstBetter(right, left)) {
-                    return right;
-                } else {
-                    return left;
-                }
-        }
-        throw new Error(`unrecognized numChild: ${numChild}`)
-    }
-}
-
 
 export default nthUglyNumber;
