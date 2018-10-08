d0be9063807207045a74852dec423538fd7aceac
htbkoo <h1017760@connect.hku.hk>
2018-10-08 19:33:52 +0800

    sped up 712_Minimum_ASCII_Delete_Sum_for_Two_Strings.ts significantly typescript

--- src/main/typescript/online/leetcode/712_Minimum_ASCII_Delete_Sum_for_Two_Strings.ts
+++ src/main/typescript/online/leetcode/712_Minimum_ASCII_Delete_Sum_for_Two_Strings.ts
@@ -32,42 +32,13 @@ All elements of each string will have an ASCII value in [97, 122].
  * @return {number}
  */
 var minimumDeleteSum = function (s1: string, s2: string): number {
-    // "97:a,98:b,99:c,100:d,101:e,102:f,103:g,104:h,105:i,106:j,107:k,108:l,109:m,110:n,111:o,112:p,113:q,114:r,115:s,116:t,117:u,118:v,119:w,120:x,121:y,122:z";
-    const MAP = {
-        a: 97,
-        b: 98,
-        c: 99,
-        d: 100,
-        e: 101,
-        f: 102,
-        g: 103,
-        h: 104,
-        i: 105,
-        j: 106,
-        k: 107,
-        l: 108,
-        m: 109,
-        n: 110,
-        o: 111,
-        p: 112,
-        q: 113,
-        r: 114,
-        s: 115,
-        t: 116,
-        u: 117,
-        v: 118,
-        w: 119,
-        x: 120,
-        y: 121,
-        z: 122
-    };
     const length1 = s1.length, length2 = s2.length;
 
     let lastRow = new Array(length1);
     lastRow.unshift(0);
 
-    let s1Ascii = s1.split("").map(ch => MAP[ch]),
-        s2Ascii = s2.split("").map(ch => MAP[ch]);
+    let s1Ascii = s1.split("").map(ch => ch.charCodeAt(0)),
+        s2Ascii = s2.split("").map(ch => ch.charCodeAt(0));
     for (let i = 1; i <= length1; ++i) {
         lastRow[i] = s1Ascii[i - 1] + lastRow[i - 1];
     }
@@ -80,7 +51,7 @@ var minimumDeleteSum = function (s1: string, s2: string): number {
             if (char1 === char2) {
                 currentRow[j] = lastRow[j - 1];
             } else {
-                currentRow[j] = Math.min(currentRow[j - 1] + MAP[char1], lastRow[j] + MAP[char2]);
+                currentRow[j] = Math.min(currentRow[j - 1] + char1.charCodeAt(0), lastRow[j] + char2.charCodeAt(0));
             }
         }
         lastRow = currentRow;
