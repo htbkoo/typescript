bd6c9c19a1fe0cb816d3ad2fcb2133043fed9ace
htbkoo <h1017760@connect.hku.hk>
2018-12-05 22:16:10 +0800

    created promise.test.ts to strengthen my knowledge of promise (reference: 1. https://medium.com/appsflyer/unlocking-the-javascript-code-interview-an-interviewer-perspective-f4fe06246b29 2. https://pouchdb.com/2015/05/18/we-have-a-problem-with-promises.html ) typescript

---
+++ src/testMocha/typescript/practices/js_language_features/promise.test.ts
@@ -0,0 +1,62 @@
+describe("promise", function () {
+    it('test that promise is executed once created', function (done) {
+        promiseLogAndResolve("tried function");
+        new Promise(resolve => {
+            console.log("executed once created");
+            resolve();
+        });
+
+        done();
+    });
+
+    describe('should be able to notify using promise to fulfill Question 4', function () {
+        // Reference: https://medium.com/appsflyer/unlocking-the-javascript-code-interview-an-interviewer-perspective-f4fe06246b29
+        // Question 4: Write 2 functions, which receive an array of functions as an input, and notify using a promise,
+        // when they are all complete. Each of the input functions returns a promise and resolves it when done.
+        // Your first function should execute all the functions in the input array, without any importance for their order,
+        // and the second one should execute each after the previous ended.
+        // Both should notify its caller using a promise when they are done.
+
+        const functions = [
+            () => promiseLogAndResolve(0),
+            () => new Promise(resolve => setInterval(() => resolve(1), 200)),
+            () => promiseLogAndResolve(2),
+            () => new Promise(resolve => setInterval(() => resolve(3), 400)),
+            () => promiseLogAndResolve(4),
+        ];
+
+        it("should execute all", function () {
+            // given
+            function executeAll(fns) {
+                return Promise.all(fns.map(fn => fn()));
+            }
+
+            // when
+            // then
+            return executeAll(functions);
+        });
+        it('should execute sequentially', function () {
+            // given
+            function executeInOrder(fns) {
+                return fns.reduce((prev, curr) => prev.then(() => curr), Promise.resolve());
+            }
+
+            // when
+            // then
+            return executeInOrder(functions);
+        });
+    });
+
+    function promiseLogAndResolve(val) {
+        console.log(`promise returning ${val}`);
+        return Promise.resolve(val);
+    }
+
+    function waitAndLogAndResolve(timeout, val) {
+        console.log(`wait ${val}`);
+        return new Promise(resolve => setInterval(() => {
+            console.log(`resolving after wait ${val}`);
+            return resolve(val);
+        }, timeout));
+    }
+});
