536d2242186761923d92048a7315561e81d86154
htbkoo <h1017760@connect.hku.hk>
2018-12-07 20:18:28 +0800

    updated promise.ts typescript

--- src/testMocha/typescript/practices/elementsProgrammingInterview/bitsManipulation.ts
+++ src/testMocha/typescript/practices/elementsProgrammingInterview/bitsManipulation.ts
@@ -3,6 +3,17 @@ import * as chai from "chai";
 describe("bits manipulation", function () {
     [
         {input: 0, expected: 0},
+        {input: 1, expected: 1},
+        {input: 2, expected: 2},
+        {input: 3, expected: 1},
+        {input: 4, expected: 4},
+        {input: 5, expected: 1},
+        {input: 6, expected: 2},
+        {input: 7, expected: 1},
+        {input: 8, expected: 8},
+        {input: 15, expected: 1},
+        {input: 16, expected: 16},
+        {input: 100, expected: 4},
     ].forEach(({input, expected}) =>
         it("should get the rightmost bit that is set to 1", function () {
             // given
