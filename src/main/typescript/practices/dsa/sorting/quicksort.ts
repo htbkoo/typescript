e944ad4e3e477395491579f6855cc73c4224cc10
htbkoo <h1017760@connect.hku.hk>
2019-01-19 11:22:28 +0800

    (WIP) updated signature for `quickSort()` to allow overriding of the comparatorFunction; fixed `typescript` transpilation error (reference: https://stackoverflow.com/a/54250121/10734272) typescript

--- src/main/typescript/practices/dsa/sorting/quicksort.ts
+++ src/main/typescript/practices/dsa/sorting/quicksort.ts
@@ -1,3 +1,7 @@
-export function quickSort<T>(arr: Array<T>): Array<T> {
+const DEFAULT_COMPARATOR = (a, b) => a - b;
+
+export type Comparator<T> = (a: T, b: T) => number;
+
+export function quickSort<T>(arr: Array<T>, compareFn: Comparator<T> = DEFAULT_COMPARATOR): Array<T> {
     return arr;
 }
--- src/testMocha/typescript/practices/dsa/sorting/quicksort.test.ts
+++ src/testMocha/typescript/practices/dsa/sorting/quicksort.test.ts
@@ -4,15 +4,27 @@ import {quickSort} from "../../../../../main/typescript/practices/dsa/sorting/qu
 describe("quick sort", function () {
     [
         {inputArray: [], expected: [], confirmUnmodified: []},
-    ].forEach(({inputArray, expected, confirmUnmodified}) =>
+        {inputArray: [1], expected: [1], confirmUnmodified: [1]},
+        {inputArray: [1, 2], expected: [1, 2], confirmUnmodified: [1, 2]},
+        {inputArray: [2, 1], expected: [1, 2], confirmUnmodified: [2, 1]},
+        {inputArray: [2, 5, 3, 4, 1], expected: [1, 2, 3, 4, 5], confirmUnmodified: [2, 5, 3, 4, 1]},
+        {
+            inputArray: ["b", "a"],
+            expected: ["a", "b"],
+            confirmUnmodified: ["b", "a"],
+            compareFn: (str1, str2) => ((str1 == str2) ? 0 : ((str1 > str2) ? 1 : -1))
+        },
+    ].forEach(({inputArray, expected, confirmUnmodified, compareFn}) =>
         it(`should sort the generic array ${JSON.stringify(inputArray)}`, function () {
             // given
             // when
-            let actual = quickSort(inputArray);
+            let actual = compareFn
+                ? quickSort<string | number>(inputArray, compareFn)
+                : quickSort<string | number>(inputArray);
 
             // then
-            chai.expect(actual).to.deep.equal(expected);
-            chai.expect(inputArray).to.deep.equal(confirmUnmodified);
+            chai.expect(actual).to.deep.equal(expected, "array should be sorted");
+            chai.expect(inputArray).to.deep.equal(confirmUnmodified, "order of original array should not be changed");
         })
     );
 });
