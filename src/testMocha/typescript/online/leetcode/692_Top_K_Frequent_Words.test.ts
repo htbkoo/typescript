9b0bd57539ab3d904b8b6e16361ea38786fabba7
htbkoo <h1017760@connect.hku.hk>
2019-01-23 19:07:23 +0800

    solved 692_Top_K_Frequent_Words.ts typescript

--- src/testMocha/typescript/online/leetcode/692_Top_K_Frequent_Words.test.ts
+++ src/testMocha/typescript/online/leetcode/692_Top_K_Frequent_Words.test.ts
@@ -14,10 +14,52 @@ describe("692. Top K Frequent Words", function () {
             },
             {
                 input: {
+                    words: ["i", "love", "leetcode", "i", "love", "coding"],
+                    k: 1,
+                },
+                expected: ["i",]
+            },
+            {
+                input: {
+                    words: ["i", "love", "leetcode", "i", "love", "coding"],
+                    k: 3,
+                },
+                expected: ["i", "love", "coding"]
+            },
+            {
+                input: {
+                    words: ["i", "love", "leetcode", "i", "love", "coding"],
+                    k: 4,
+                },
+                expected: ["i", "love", "coding", "leetcode"]
+            },
+            {
+                input: {
                     words: ["the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"],
                     k: 4,
                 },
                 expected: ["the", "is", "sunny", "day"]
+            },
+            {
+                input: {
+                    words: ["the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"],
+                    k: 1,
+                },
+                expected: ["the",]
+            },
+            {
+                input: {
+                    words: ["the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"],
+                    k: 2,
+                },
+                expected: ["the", "is",]
+            },
+            {
+                input: {
+                    words: ["the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"],
+                    k: 3,
+                },
+                expected: ["the", "is", "sunny",]
             },
         ].forEach(({input, expected}) =>
             it(`topKFrequent(${JSON.stringify(input)}`, function () {
