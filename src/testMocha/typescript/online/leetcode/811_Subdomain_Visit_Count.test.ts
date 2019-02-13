20ce66d579d4992e1192a986fa6f170425fd7347
htbkoo <h1017760@connect.hku.hk>
2019-02-13 08:57:41 +0800

    WIP 811_Subdomain_Visit_Count.ts typescript

---
+++ src/testMocha/typescript/online/leetcode/811_Subdomain_Visit_Count.test.ts
@@ -0,0 +1,48 @@
+import * as chai from "chai";
+
+import subdomainVisits from "../../../../main/typescript/online/leetcode/811_Subdomain_Visit_Count";
+import {toUrlFreqMap, getFreqPair} from "../../../../main/typescript/online/leetcode/811_Subdomain_Visit_Count";
+
+describe("811. Subdomain Visit Count", function () {
+    describe('should count subdomain visit', function () {
+        [
+            {
+                input: {cpdomains: ["9001 discuss.leetcode.com"]},
+                expected: ["9001 discuss.leetcode.com", "9001 leetcode.com", "9001 com"]
+            },
+            {
+                input: {cpdomains: ["900 google.mail.com", "50 yahoo.com", "1 intel.mail.com", "5 wiki.org"]},
+                expected: ["901 mail.com","50 yahoo.com","900 google.mail.com","5 wiki.org","5 org","1 intel.mail.com","951 com"]
+            },
+        ].forEach(({input, expected}) =>
+            it(`subdomainVisits(${JSON.stringify(input)}`, function () {
+                // given
+                const {cpdomains} = input;
+
+                // when
+                const actual = subdomainVisits(cpdomains);
+
+                // then
+                chai.expect(actual).to.deep.equal(expected);
+            })
+        );
+    });
+
+    describe("toUrlFreqMap", function () {
+
+    });
+
+    describe("getFreqPair", function () {
+        it("should map to freq pair", function () {
+            // given
+            const cpdomain = "9001 discuss.leetcode.com";
+
+            // when
+            const pair = getFreqPair(cpdomain);
+
+            // then
+            const expected = ["discuss.leetcode.com", 9001];
+            chai.expect(pair).to.deep.equal(expected);
+        });
+    });
+});
