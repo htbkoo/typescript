0431668b3854defcfef050772209faf2f6366ab4
htbkoo <h1017760@connect.hku.hk>
2019-03-22 19:36:30 +0800

    solved BigSorting.ts typescript

---
+++ src/main/typescript/online/hackerRank/practice/algorithms/warmup/BigSorting.ts
@@ -0,0 +1,61 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString: any = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the bigSorting function below.
+function bigSorting(unsorted: string[]): string[] {
+    return unsorted.sort((a, b) => {
+        if (a.length === b.length) {
+            for (let i = 0; i < a.length; ++i) {
+                if (a.charAt(i) !== b.charAt(i)) {
+                    return parseInt(a.charAt(i)) - parseInt(b.charAt(i));
+                }
+            }
+        } else {
+            return a.length - b.length;
+        }
+    });
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    let unsorted = [];
+
+    for (let i = 0; i < n; i++) {
+        const unsortedItem = readLine();
+        unsorted.push(unsortedItem);
+    }
+
+    let result = bigSorting(unsorted);
+
+    ws.write(result.join("\n") + "\n");
+
+    ws.end();
+}
+
+export {bigSorting};
