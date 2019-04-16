e82b0f6c025ac2c4137eda6b58ae3b7eb4875864
htbkoo <h1017760@connect.hku.hk>
2019-04-16 19:08:04 +0800

    imported test cases from HeapSort tests; refactored SortingEngineTestUtils.ts to allow easy toggling of enabled test suites

--- src/testMocha/typescript/practices/dsa/sorting/utils/SortingEngineTestUtils.ts
+++ src/testMocha/typescript/practices/dsa/sorting/utils/SortingEngineTestUtils.ts
@@ -1,5 +1,6 @@
 import * as chai from "chai";
 import SortingEngine from "../../../../../../main/typescript/practices/dsa/sorting/SortingEngine";
+import rawHeapTestCases from "../../heap/resources/heapTestCases.json";
 
 type T = string | number;
 
@@ -12,12 +13,12 @@ interface TestCaseParams {
 
 const NO_ARGS = [];
 
-const DEFAULT_TEST_CASES_PARAMS = [
+const DEFAULT_TEST_CASES_PARAMS: TestCaseParams[] = [
+    {inputArray: [2, 5, 3, 4, 1], expected: [1, 2, 3, 4, 5], confirmUnmodified: [2, 5, 3, 4, 1]},
     {inputArray: [], expected: [], confirmUnmodified: []},
     {inputArray: [1], expected: [1], confirmUnmodified: [1]},
     {inputArray: [1, 2], expected: [1, 2], confirmUnmodified: [1, 2]},
     {inputArray: [2, 1], expected: [1, 2], confirmUnmodified: [2, 1]},
-    {inputArray: [2, 5, 3, 4, 1], expected: [1, 2, 3, 4, 5], confirmUnmodified: [2, 5, 3, 4, 1]},
     {inputArray: [3, 5, 1, 4, 2, 6], expected: [1, 2, 3, 4, 5, 6], confirmUnmodified: [3, 5, 1, 4, 2, 6]},
     {inputArray: [3, 5, 1, 7, 4, 2, 6], expected: [1, 2, 3, 4, 5, 6, 7], confirmUnmodified: [3, 5, 1, 7, 4, 2, 6]},
     {
@@ -28,18 +29,40 @@ const DEFAULT_TEST_CASES_PARAMS = [
     },
 ];
 
-export function runDefaultTestCases(EngineClass: Function, argumentsList: ArrayLike<any> = NO_ARGS) {
-    return DEFAULT_TEST_CASES_PARAMS.forEach(({inputArray, expected, confirmUnmodified, compareFn}: TestCaseParams) =>
-        it(`should sort the generic array ${JSON.stringify(inputArray)} with ${EngineClass.name}`, function () {
-            // given
-            const engine: SortingEngine = Reflect.construct(EngineClass, argumentsList);
+const heapSortTestCasesParams: TestCaseParams[] = rawHeapTestCases.map(({input, expected}) => ({
+    inputArray: input,
+    expected,
+    confirmUnmodified: input.slice()
+}));
 
-            // when
-            const actual = engine.sort(inputArray, compareFn);
+const TestsParams = {
+    DEFAULT: DEFAULT_TEST_CASES_PARAMS,
+    HEAP_SORT: heapSortTestCasesParams
+};
 
-            // then
-            chai.expect(actual).to.deep.equal(expected, "array should be sorted");
-            chai.expect(inputArray).to.deep.equal(confirmUnmodified, "order of original array should not be changed");
-        })
+type TestSuiteType = keyof typeof TestsParams;
+
+type Options = { argumentsList?: ArrayLike<any>, enabledTestSuite?: Array<TestSuiteType> };
+
+export function runDefaultTestCases(EngineClass: Function, options?: Options) {
+    const {argumentsList, enabledTestSuite} = {argumentsList: NO_ARGS, enabledTestSuite: ["DEFAULT",], ...options};
+
+    enabledTestSuite.forEach(
+        suiteName =>
+            describe(`TestSuite - ${suiteName}`, function () {
+                TestsParams[suiteName].forEach(({inputArray, expected, confirmUnmodified, compareFn}: TestCaseParams) =>
+                    it(`should sort the generic array ${JSON.stringify(inputArray)} with ${EngineClass.name}`, function () {
+                        // given
+                        const engine: SortingEngine = Reflect.construct(EngineClass, argumentsList);
+
+                        // when
+                        const actual = engine.sort(inputArray, compareFn);
+
+                        // then
+                        chai.expect(actual).to.deep.equal(expected, "array should be sorted");
+                        chai.expect(inputArray).to.deep.equal(confirmUnmodified, "order of original array should not be changed");
+                    })
+                )
+            })
     );
 }
