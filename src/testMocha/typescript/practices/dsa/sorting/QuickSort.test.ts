e6a977691f8c97a891c160d665a1d98bce943352
htbkoo <h1017760@connect.hku.hk>
2019-04-16 15:38:55 +0800

    renamed util file for clarity

--- src/testMocha/typescript/practices/dsa/sorting/QuickSort.test.ts
+++ src/testMocha/typescript/practices/dsa/sorting/QuickSort.test.ts
@@ -1,5 +1,5 @@
 import {QuickSort} from "../../../../../main/typescript/practices/dsa/sorting/quicksort";
-import {runDefaultTestCases} from "./SortingEngine.test";
+import {runDefaultTestCases} from "./SortingEngineTestUtils";
 
 describe("quick sort", function () {
     runDefaultTestCases(QuickSort);
