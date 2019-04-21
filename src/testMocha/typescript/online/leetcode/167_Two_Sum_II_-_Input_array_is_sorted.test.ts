248e16120986667bd3c00aa851ea0f62354017c5
htbkoo <h1017760@connect.hku.hk>
2019-04-21 12:03:48 +0800

    solved `167_Two_Sum_II_-_Input_array_is_sorted.ts`

---
+++ src/testMocha/typescript/online/leetcode/167_Two_Sum_II_-_Input_array_is_sorted.test.ts
@@ -0,0 +1,26 @@
+import * as chai from "chai";
+
+import twoSum from "../../../../main/typescript/online/leetcode/167_Two_Sum_II_-_Input_array_is_sorted";
+
+describe("167. Two Sum II - Input array is sorted", function () {
+    describe('should return the minimum sum of a falling path through A', function () {
+        [
+            {input: {numbers: [5, 25, 75], target: 100}, output: [2, 3]},
+            {input: {numbers: [0, 1, 3, 4, 10], target: 10}, output: [1, 5]},
+            {input: {numbers: [0, 1, 3, 4], target: 3}, output: [1, 3]},
+            {input: {numbers: [0, 1, 3, 4], target: 1}, output: [1, 2]},
+            {input: {numbers: [2, 7, 11, 15], target: 9}, output: [1, 2]},
+            {input: {numbers: [0, 0, 3, 4], target: 0}, output: [1, 2]},
+        ].forEach(({input, output}) =>
+            it(`twoSum(${JSON.stringify(input)})=${JSON.stringify(output)}`, function () {
+                // given
+                const {numbers, target} = input;
+                // when
+                const actual = twoSum(numbers, target);
+
+                // then
+                chai.expect(actual).to.deep.equal(output);
+            })
+        );
+    });
+});
