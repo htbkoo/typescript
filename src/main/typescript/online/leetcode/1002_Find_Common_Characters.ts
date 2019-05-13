f0a02b3b70a31dd280f9fc4142c33cafc86e488d
htbkoo <h1017760@connect.hku.hk>
2019-05-13 18:47:12 +0800

    solved 1002_Find_Common_Characters.ts

---
+++ src/main/typescript/online/leetcode/1002_Find_Common_Characters.ts
@@ -0,0 +1,59 @@
+/*
+https://leetcode.com/problems/find-common-characters/
+
+Given an array A of strings made only from lowercase letters, return a list of all characters that show up in all strings within the list (including duplicates).  For example, if a character occurs 3 times in all strings but not 4 times, you need to include that character three times in the final answer.
+
+You may return the answer in any order.
+
+
+
+Example 1:
+
+Input: ["bella","label","roller"]
+Output: ["e","l","l"]
+
+Example 2:
+
+Input: ["cool","lock","cook"]
+Output: ["c","o"]
+
+
+
+Note:
+
+    1 <= A.length <= 100
+    1 <= A[i].length <= 100
+    A[i][j] is a lowercase letter
+
+* */
+
+/**
+ * @param {string[]} A
+ * @return {string[]}
+ */
+var commonChars = function (A: string[]): string[] {
+    const commonFreq = A.splice(1).map(countCharFreq)
+        .reduce((obj, curr) => {
+            return Object.keys(curr).reduce((o, ch) => {
+                if (ch in obj) {
+                    o[ch] = Math.min(obj[ch], curr[ch]);
+                }
+                return o;
+            }, {});
+        }, countCharFreq(A[0]));
+
+    return Object.keys(commonFreq).reduce((arr, ch) => arr.concat(ch.repeat(commonFreq[ch]).split("")), []);
+
+    function countCharFreq(str: string): { [ch: string]: number } {
+        return str.split("").reduce((obj, char) => {
+            if (char in obj) {
+                obj[char]++;
+            } else {
+                obj[char] = 1;
+            }
+            return obj;
+        }, {});
+    }
+};
+
+export default commonChars;
---
+++ src/testMocha/typescript/online/leetcode/1002_Find_Common_Characters.test.ts
@@ -0,0 +1,22 @@
+import * as chai from "chai";
+
+import commonChars from "../../../../main/typescript/online/leetcode/1002_Find_Common_Characters";
+
+describe("1002. Find Common Characters", function () {
+    describe('should return a list of all characters that show up in all strings within the list (including duplicates)', function () {
+        [
+            {input: ["bella", "label", "roller"], output: ["e", "l", "l"]},
+            {input: ["cool", "lock", "cook"], output: ["c", "o"]},
+        ].forEach(({input, output}) =>
+            it(`commonChars(${JSON.stringify(input)})=${JSON.stringify(output)}`, function () {
+                // given
+                // when
+                const actual = commonChars(input);
+
+                // then
+
+                chai.expect(actual).to.deep.equal(output);
+            })
+        );
+    });
+});
